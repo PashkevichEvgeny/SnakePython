7990bae8c18dceefddd58771977ba73fbee1d00b
PashkevichEvgeny <pash.ev@ya.ru>
2023-11-21 14:38:48 +0800

    Рисуем точку

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import curses
+# Для работы с консолью надо установить библиотеку windows-curses, она будет отрисовывать символы
+# pip install windows-curses
+# В случае ошибки "Redirection is not supported."
+# установите галочку в Run -> EditConfigurations -> Emulate terminal in output console
+
+
+def canvas():
+    """
+     Функция создает и возвращает полотно, на котором будут отрисовываться символы
+    """
+    # Инициализация полотна
+    new_canvas = curses.initscr()
+    # Выключить отображение курсора
+    curses.curs_set(False)
+    return new_canvas
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    canvas = canvas()
+
+    x1 = 1
+    y1 = 3
+    sym1 = '*'
+
+    # Рисуем символ в заданных координатах
+    canvas.addstr(y1, x1, sym1)
+
+    # Обновляем полотно
+    canvas.refresh()
+
+    # Устанавлиаем задержку 3000 мс., чтобы просмотреть результат отрисовки
+    curses.napms(3000)
+
+
+
