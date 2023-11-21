612d3c3f54adcf12e97299fbc37119f871d0a946
PashkevichEvgeny <pash.ev@ya.ru>
2023-11-21 14:52:06 +0800

    Процедурный подход. Функция draw()

--- main.py
+++ main.py
@@ -16,24 +16,29 @@ def canvas():
     return new_canvas
 
 
+def draw(canvas_, x, y, sym):
+    """
+    Функция принимает полотно, координаты точки и символ, который потом отрисовывает
+    """
+    # Рисуем символ в заданных координатах
+    canvas_.addstr(y, x, sym)
+
+    # Обновляем полотно
+    canvas.refresh()
+
+
 if __name__ == '__main__':
     canvas = canvas()
 
     x1 = 1
     y1 = 3
     sym1 = '*'
+    draw(canvas, x1, y1, sym1)
 
     x2 = 4
     y2 = 5
     sym2 = '#'
-
-    # Рисуем символ в заданных координатах
-    canvas.addstr(y1, x1, sym1)
-    # Рисуем еще один символ
-    canvas.addstr(y2, x2, sym2)
-
-    # Обновляем полотно
-    canvas.refresh()
+    draw(canvas, x2, y2, sym2)
 
     # Устанавлиаем задержку 3000 мс., чтобы просмотреть результат отрисовки
     curses.napms(3000)
