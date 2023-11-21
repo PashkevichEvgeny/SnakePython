e73b6842fa0610fc63bd3e7ac686f25006c34be9
PashkevichEvgeny <pash.ev@ya.ru>
2023-11-21 23:33:13 +0800

    Создали базовый класс Figure

--- horizontal_line.py
+++ horizontal_line.py
@@ -2,9 +2,10 @@ from figure import Figure
 from point import Point
 
 
+# Наследуем класс HorizontalLine от класса Figure
 class HorizontalLine(Figure):
     def __init__(self, canvas, x_left, x_right, y, sym):
-
+        # Вызываем конструктор из базового класса
         super().__init__()
 
         for x in range(x_left, x_right + 1):
--- vertical_line.py
+++ vertical_line.py
@@ -2,9 +2,10 @@ from figure import Figure
 from point import Point
 
 
+# Наследуем класс VerticalLine от класса Figure
 class VerticalLine(Figure):
     def __init__(self, canvas, y_up, y_down, x, sym):
-
+        # Вызываем конструктор из базового класса
         super().__init__()
 
         for y in range(y_up, y_down + 1):
